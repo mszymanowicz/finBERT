b6ace0d3fe72467d946c45390c67d9cf34c8337a
Kamil <kamild973@gmail.com>
2021-06-13 21:30:00 +0200

    Add images icon and background color

--- window_app/app.py
+++ window_app/app.py
@@ -1,8 +1,8 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from finBERT.finbert.finbert import predict
-from pytorch_pretrained_bert.modeling import BertForSequenceClassification
+from PyQt5 import QtGui
+
 
 import os
 import sys
@@ -21,12 +21,13 @@ class MainWindow(QMainWindow):
         fixedfont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
         fixedfont.setPointSize(12)
         self.editor.setFont(fixedfont)
+        self.editor.setStyleSheet("color: black; background-color: #fff09c")
+        self.editor.setMaximumHeight(100)
         # self.path holds the path of the currently open file.
         # If none, we haven't got a file open yet (or creating new).
         self.listView = QTableView()
         self.model = QStandardItemModel()
         self.model.setHorizontalHeaderLabels(["Sentence", "prediction", "sentiment score"])
-
         layout.addWidget(self.editor)
         layout.addWidget(self.listView)
 
@@ -34,6 +35,9 @@ class MainWindow(QMainWindow):
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+        container.setAutoFillBackground(False)
+        container.setStyleSheet(os.path.join('images', 'background.png'))
+        #self.setFixedSize(1920, 1080)
 
         self.status = QStatusBar()
         self.setStatusBar(self.status)
@@ -42,16 +46,19 @@ class MainWindow(QMainWindow):
         file_toolbar.setIconSize(QSize(20, 20))
         self.addToolBar(file_toolbar)
 
-        open_file_action = QAction(QIcon(os.path.join('images', 'blue-folder-open-document.png')), "Open file...", self)
+        open_file_action = QAction(QIcon(os.path.join('images', 'folder_icon.png')), "Open file...", self)
         open_file_action.setStatusTip("Open file")
         open_file_action.triggered.connect(self.file_open)
         file_toolbar.addAction(open_file_action)
 
-        run_action = QAction(QIcon(os.path.join('images', 'blue-folder-open-document.png')), "Open file...", self)
+        run_action = QAction(QIcon(os.path.join('images', 'start_icon.png')), "Run ", self)
         run_action.setStatusTip("Open file")
         run_action.triggered.connect(self.click_run_button)
         file_toolbar.addAction(run_action)
 
+        self.setWindowIcon(QtGui.QIcon("images/logo_transparent.png"))
+        self.setStyleSheet("color: black; background-color: white")
+        self.resize(800, 800)
         self.show()
     def file_open(self):
         path, _ = QFileDialog.getOpenFileName(self, "Open file", "", "Text documents (*.txt);All files (*.*)")
@@ -69,10 +76,7 @@ class MainWindow(QMainWindow):
                 self.editor.setPlainText(text)
 
     def click_run_button(self):
-        model = BertForSequenceClassification.from_pretrained('/src/models/classifier_model/finbert-sentiment',
-                                                              num_labels=3, cache_dir=None)
         text = self.editor
-        print(predict(text, model).to_json(orient='records'))
         sentence = QStandardItem()
         sentence_now = self.editor.toPlainText()
         sentence.setText(sentence_now)
@@ -90,11 +94,6 @@ class MainWindow(QMainWindow):
         self.model.sort(0)
 
         self.listView.setModel(self.model)
-
-    def check_check_state(self, i):
-        if not i.isCheckable():  # Skip data columns.
-            return
-
 
 if __name__ == '__main__':
 
